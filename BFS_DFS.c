55220f14495538b990cb11fddde67331a3b2e87f
LeeDW <dawoon0513@gmail.com>
2021-10-08 16:25:53 +0900

    BFS와 DFS 수정

--- BFS_DFS.c
+++ BFS_DFS.c
@@ -14,39 +14,39 @@ int head=0,tail=0,pos=0;
 
 void BFS(TreeNode* root){
     if(root != NULL){
-        queue[tail++]=root->left;
-        queue[tail++]=root->right;
+        if(root->left!=NULL)
+            queue[tail++]=root->left;
+        if(root->right!=NULL)
+            queue[tail++]=root->right;
         printf("--%d\n",root->data);
         if(queue[head]!=NULL){
             BFS(queue[head++]);
-        }
-        else{
-            BFS(queue[++head]);
         }
     }
     
 }
 void DFS(TreeNode* root){
 if(root != NULL){
+    if(root->left!=NULL)
         stack[pos++]=root->left;
+    if(root->right!=NULL)
         stack[pos++]=root->right;
-        printf("--%d\n",root->data);
-        if(stack[head]!=NULL){
-            DFS(stack[pos--]);
-        }
-        else{
-            DFS(stack[--pos]);
+    printf("--%d\n",root->data);
+    if(stack[pos-1]!=NULL){
+        pos--;
+        DFS(stack[pos]);
         }
     }
-    
 }
 
 int main(){
-    TreeNode *n1,*n2,*n3;
+    TreeNode *n1,*n2,*n3,*n4,*n5;
     //노드 생성
     n1 = (TreeNode*)malloc(sizeof(TreeNode));
     n2 = (TreeNode*)malloc(sizeof(TreeNode));
     n3 = (TreeNode*)malloc(sizeof(TreeNode));
+    n4 = (TreeNode*)malloc(sizeof(TreeNode));
+    n5 = (TreeNode*)malloc(sizeof(TreeNode));
 
     n1->data = 10;
     n1->left = n2;
@@ -55,8 +55,15 @@ int main(){
     n2->left = NULL;
     n2->right = NULL;
     n3->data = 30;
-    n3->left = NULL;
-    n3->right = NULL;
+    n3->left = n4;
+    n3->right = n5;
+    n4->data = 11;
+    n4->left = NULL;
+    n4->right = NULL;
+    n5->data = 12;
+    n5->left = NULL;
+    n5->right = NULL;
+
 
     printf("===== BFS =====");
     printf("\n--root");
